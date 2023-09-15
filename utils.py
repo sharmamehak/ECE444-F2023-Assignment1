fcaa9aedff06b5ed7f6bebb7b2dc7e3d6feaf57f
sharmamehak <61470782+sharmamehak@users.noreply.github.com>
2023-09-14 21:07:26 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,11 +1,6 @@
 class utils: 
   def reversed(num):
-    s=len(num)
-    newnum=[None]*length
-    for i in num:
-        s=s-1
-        newnum[s]=i
-        return newnum
+    return int(str(num)[::-1])
   def formatter(num):
     r1 = bin(num)
     r2 = oct(num)
